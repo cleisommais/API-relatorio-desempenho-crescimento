9bcd355d89ad56e287864ac564f1c286af950790
Cleison Ferreira de Melo <cleisommais@gmail.com>
2020-01-23 21:14:23 +0000

    Fixing deprecated message when run the app

--- app.js
+++ app.js
@@ -5,9 +5,24 @@ import logger from "morgan";
 import reportRouter from "./routes/reportRouter";
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connect(process.env.MONGODB_URI,{
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500, // Reconnect every 500ms
+    connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "Erro de conexão."))
+db.once('open', function() {
+    console.log('MongoDB connection opened!');
+});
+db.on('reconnected', function() {
+    console.log('MongoDB reconnected!');
+});
 
 app.use(logger('dev'));
 app.use(express.json());
